b68cd38c6f2c67577cf7331963a84cf8502682d4
xil-7 <auxiliack@gmail.com>
2026-02-08 09:16:07 +0700

    Add some improvement for order history

--- makanapa.sql
+++ makanapa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 08, 2026 at 03:02 AM
+-- Generation Time: Feb 08, 2026 at 03:14 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -37,18 +37,6 @@ CREATE TABLE `offers` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `offers`
---
-
-INSERT INTO `offers` (`id`, `request_id`, `seller_name`, `food_name`, `price`, `contact`, `created_at`) VALUES
-(1, 1, 'Seetty', 'aasss', 10000, '089529129124', '2026-02-08 00:54:56'),
-(2, 2, 'Seetty', 'aasss', 10000, '089529129124', '2026-02-08 01:30:15'),
-(3, 3, 'Seetty', 'aasss', 10000, '089529129124', '2026-02-08 01:32:26'),
-(4, 4, 'Seetty', 'awddawdawa', 10000, '91323239199', '2026-02-08 01:36:18'),
-(5, 5, 'Seetty', 'dwwqddwdw', 10000, 'wdvseef', '2026-02-08 01:39:09'),
-(6, 6, 'Seetty', 'qefffe', 129929, '129219912', '2026-02-08 01:49:56');
-
 -- --------------------------------------------------------
 
 --
@@ -69,16 +57,6 @@ CREATE TABLE `orders` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `orders`
---
-
-INSERT INTO `orders` (`id`, `request_id`, `buyer_name`, `buyer_address`, `seller_name`, `food_name`, `price`, `quantity`, `total`, `contact`, `created_at`) VALUES
-(1, 4, 'sads', 'Jalan Haji Umar Ismail, Kota Jababeka, Simpangan, Kab Bekasi, West Java, Java, 17533, Indonesia', 'Seetty', 'awddawdawa', 10000, 1, 10000, '91323239199', '2026-02-08 01:36:23'),
-(2, 4, 'sads', 'Jalan Haji Umar Ismail, Kota Jababeka, Simpangan, Kab Bekasi, West Java, Java, 17533, Indonesia', 'Seetty', 'awddawdawa', 10000, 2, 20000, '91323239199', '2026-02-08 01:36:35'),
-(3, 5, 'sads', 'Jalan Haji Umar Ismail, Kota Jababeka, Simpangan, Kab Bekasi, West Java, Java, 17533, Indonesia', 'Seetty', 'dwwqddwdw', 10000, 1, 10000, '', '2026-02-08 01:39:15'),
-(4, 6, 'sads', 'Jalan Haji Umar Ismail, Kota Jababeka, Simpangan, Kab Bekasi, West Java, Java, 17533, Indonesia', 'Seetty', 'qefffe', 129929, 1, 129929, '129219912', '2026-02-08 01:50:01');
-
 -- --------------------------------------------------------
 
 --
@@ -92,18 +70,6 @@ CREATE TABLE `requests` (
   `quantity` int(11) DEFAULT 1,
   `buyer_name` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `requests`
---
-
-INSERT INTO `requests` (`id`, `description`, `created_at`, `quantity`, `buyer_name`) VALUES
-(1, 'asasa', '2026-02-08 00:54:48', 1, 'sads'),
-(2, 'wdwqw', '2026-02-08 01:30:05', 1, 'sads'),
-(3, 'qdwqdwdqwdqwdqwdqdwqd', '2026-02-08 01:32:18', 1, 'sads'),
-(4, 'dwdaas', '2026-02-08 01:36:05', 1, 'sads'),
-(5, 'wqqdw', '2026-02-08 01:38:58', 1, 'sads'),
-(6, 'efqeeqe', '2026-02-08 01:49:38', 1, 'sads');
 
 -- --------------------------------------------------------
 
@@ -156,19 +122,19 @@ ALTER TABLE `user_habits`
 -- AUTO_INCREMENT for table `offers`
 --
 ALTER TABLE `offers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `requests`
 --
 ALTER TABLE `requests`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user_habits`
